07c33b6fcccfb1d0b74f09f41f8ccc65c3fa21f1
Istvan Rechner <istvan.rechner@gmail.com>
2011-12-01 23:27:41 +0100

    Done with user edit/update, index, and destroy actions

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,99 +2,97 @@ require 'spec_helper'
 
 describe UsersController do
   render_views
-  
-  describe "GET 'show'" do
-  it "should have the right title" do
-      get :show, :id => @user
-      response.should have_selector("title", :content => @user.name)
+
+  describe "GET 'index'" do
+
+    describe "for non-signed-in users" do
+      it "should deny access" do
+        get :index
+        response.should redirect_to(signin_path)
+        flash[:notice].should =~ /sign in/i
+      end
     end
 
-    it "should include the user's name" do
-      get :show, :id => @user
-      response.should have_selector("h1", :content => @user.name)
-    end
-
-    it "should have a profile image" do
-      get :show, :id => @user
-      response.should have_selector("h1>img", :class => "gravatar")
-    end
-  end
-  
-  describe "GET 'new'" do
-
-  before(:each) do
-      @user = Factory(:user)
-    end
-
-    it "should be successful" do
-      get :show, :id => @user
-      response.should be_success
-    end
-
-    it "should find the right user" do
-      get :show, :id => @user
-      assigns(:user).should == @user
-    end
-  
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'new'
-      response.should have_selector("title", :content => "Sign up")
-    end
-  end
-   describe "POST 'create'" do
-
-    describe "failure" do
+    describe "for signed-in users" do
 
       before(:each) do
-        @attr = { :name => "", :email => "", :password => "",
-                  :password_confirmation => "" }
+        @user = test_sign_in(Factory(:user))
+        second = Factory(:user, :name => "Bob", :email => "another@example.com")
+        third  = Factory(:user, :name => "Ben", :email => "another@example.net")
+
+         @users = [@user, second, third]
+        30.times do
+          @users << Factory(:user, :name => Factory.next(:name),
+                                   :email => Factory.next(:email))
+        end
       end
 
-      it "should not create a user" do
-        lambda do
-          post :create, :user => @attr
-        end.should_not change(User, :count)
+      it "should be successful" do
+        get :index
+        response.should be_success
       end
 
       it "should have the right title" do
-        post :create, :user => @attr
-        response.should have_selector("title", :content => "Sign up")
+        get :index
+        response.should have_selector("title", :content => "All users")
       end
 
-      it "should render the 'new' page" do
-        post :create, :user => @attr
-        response.should render_template('new')
-	  end
-	end
- describe "success" do
+      it "should have an element for each user" do
+        get :index
+        @users[0..2].each do |user|
+          response.should have_selector("li", :content => user.name)
+        end
+      end
+
+      it "should paginate users" do
+        get :index
+        response.should have_selector("div.pagination")
+        response.should have_selector("span.disabled", :content => "Previous")
+        response.should have_selector("a", :href => "/users?page=2",
+                                           :content => "2")
+        response.should have_selector("a", :href => "/users?page=2",
+                                           :content => "Next")
+      end
+    end
+  end
+   describe "DELETE 'destroy'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    describe "as a non-signed-in user" do
+      it "should deny access" do
+        delete :destroy, :id => @user
+        response.should redirect_to(signin_path)
+      end
+    end
+
+    describe "as a non-admin user" do
+      it "should protect the page" do
+        test_sign_in(@user)
+        delete :destroy, :id => @user
+        response.should redirect_to(root_path)
+      end
+    end
+
+    describe "as an admin user" do
 
       before(:each) do
-        @attr = { :name => "New User", :email => "user@example.com",
-                  :password => "foobar", :password_confirmation => "foobar" }
+        admin = Factory(:user, :email => "admin@example.com", :admin => true)
+        test_sign_in(admin)
       end
 
-      it "should create a user" do
+      it "should destroy the user" do
         lambda do
-          post :create, :user => @attr
-        end.should change(User, :count).by(1)
+          delete :destroy, :id => @user
+        end.should change(User, :count).by(-1)
       end
-	  it "should sign the user in" do
-        post :create, :user => @attr
-        controller.should be_signed_in
+
+      it "should redirect to the users page" do
+        delete :destroy, :id => @user
+        response.should redirect_to(users_path)
       end
-      it "should redirect to the user show page" do
-        post :create, :user => @attr
-        response.should redirect_to(user_path(assigns(:user)))
-      end   
-	  it "should have a welcome message" do
-        post :create, :user => @attr
-        flash[:success].should =~ /welcome to the sample app/i
-      end	  
     end
   end
 end
